ed0e2039fe7545416a61646dc0f6583707b2af3d
zzc <1710721370@qq.com>
2020-10-22 14:34:08 +0800

    动态头部和轮播

--- src/pages/MSite/MSite.vue
+++ src/pages/MSite/MSite.vue
@@ -2,17 +2,14 @@
   <!--首页外卖-->
   <div class="msite">
     <!--首页头部-->
-    <header class="header">
-          <span class="header_search">
-            <i class="iconfont icon-sousuo" ></i>
+    <HeaderTop title="中山火炬职业技术学院">
+          <span class="header_search" slot="left">
+            <i class="iconfont icon-icon-sousuo" ></i>
           </span>
-      <span class="header_title">
-            <span class="header_title_text ellipsis">昌平区北七家宏福科技园(337省道北)</span>
-          </span>
-      <span class="header_login">
+          <span class="header_login" slot="right">
             <span class="header_login_text">登录|注册</span>
           </span>
-    </header>
+    </HeaderTop>
     <!--首页导航-->
     <nav class="msite_nav">
       <div class="swiper-container">
@@ -313,52 +310,31 @@
 </template>
 
 <script>
-    export default {}
+  import HeaderTop from "../../components/HeaderTop/HeaderTop";
+  import Swiper from 'swiper';
+  import 'swiper/dist/css/swiper.min.css';
+    export default {
+        mounted(){
+            //创建一个Swiper实例对象，来实现轮播
+            new Swiper('.swiper-container',{
+                loop:true,//实现循环轮播·
+                //如果需要分页器
+                pagination:{
+                    el:'.swiper-pagination'
+                }
+
+            })
+        },
+        components:{
+            HeaderTop
+        }
+    }
 </script>
 
 <style lang="stylus" ref="stylesheet/stylus">
   @import "../../common/stylus/mixins.styl";
   .msite  //首页
     width 100%
-    .header
-      background-color #02a774
-      position fixed
-      z-index 100
-      left 0
-      top 0
-      width 100%
-      height 45px
-      .header_search
-        position absolute
-        left 15px
-        top 50%
-        transform translateY(-50%)
-        width 10%
-        height 50%
-        .icon-sousuo
-          font-size 25px
-          color #fff
-      .header_title
-        position absolute
-        top 50%
-        left 50%
-        transform translate(-50%, -50%)
-        width 50%
-        color #fff
-        text-align center
-        .header_title_text
-          font-size 20px
-          color #fff
-          display block
-      .header_login
-        font-size 14px
-        color #fff
-        position absolute
-        right 15px
-        top 50%
-        transform translateY(-50%)
-        .header_login_text
-          color #fff
     .msite_nav
       bottom-border-1px(#e4e4e4)
       margin-top 45px
